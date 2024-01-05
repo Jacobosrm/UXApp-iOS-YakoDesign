4422e9cc601c10a244d49bb787694af6e1259082
Jacobo <jacobosramirezm@gmail.com>
2024-01-05 10:40:21 -0500

    Initial Commit

---
+++ UXApp-iOS-YakoDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UXApp-iOS-YakoDesign
+//
+//  Created by Jacobo Ramirez on 5/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
